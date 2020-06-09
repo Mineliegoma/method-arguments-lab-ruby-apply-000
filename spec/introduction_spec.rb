2bfe996c275f1e717dc358c85b65a1900ff983ac
adept-bell-4492 <garlandegoma@gmail.com>
2020-06-09 13:09:07 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -4,8 +4,7 @@ def introduction(name)
 puts "Hi, my name is #{name}."
 
 
-#def introduction_with_language( name, language ) 
- #puts "Hi, my name is #{name} and i am learning to program in #{language}."
+
 
 
 describe "#introduction" do 
