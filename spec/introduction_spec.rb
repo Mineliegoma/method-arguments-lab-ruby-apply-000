a6d91f4baf78aea58c92e5ecef983d8c9e76017d
adept-bell-4492 <garlandegoma@gmail.com>
2020-06-09 13:06:24 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -11,7 +11,9 @@ require "spec_helper"
 describe "#introduction" do 
  it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
     expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
-end
+  end
+end 
+
 
 describe "#introduction_with_language" do 
   it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do 
